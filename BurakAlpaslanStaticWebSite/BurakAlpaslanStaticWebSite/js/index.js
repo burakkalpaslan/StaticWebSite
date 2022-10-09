76e88a2c9128b577327bf27a932d0acd51927449
burakkalpaslan <burakkalpaslan@gmail.com>
2022-10-09 16:18:56 +0300

    StaticWebSite

---
+++ BurakAlpaslanStaticWebSite/BurakAlpaslanStaticWebSite/js/index.js
@@ -0,0 +1,8 @@
+﻿//var navdiv = document.getElementById("navdiv");
+//var parentDiv = navdiv.parentNode;
+//this.parentDiv.style.opacity = 0.4;
+//this.navdiv.style.opacity = 1;
+
+//function myFunction(x) {
+//    x.style.background = "yellow";
+//}
